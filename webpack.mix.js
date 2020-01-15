7a337f20b03f4039db3802f360fe2a50cc07ca79
Zorca Orcinus <vs@zorca.org>
2020-01-15 20:55:48 +0300

    Adjust assets paths

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+mix
+    .js('assets/scripts/app.js', 'public/')
+    .sass('assets/styles/app.scss', 'public/');
 
 // Full API
 // mix.js(src, output);
